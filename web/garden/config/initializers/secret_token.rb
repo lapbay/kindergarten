8eda3353b9e6e4d90103f1e0e9db9f603c82329a
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2012-10-07 14:27:12 +0200

    rename iOS project to kindergarten

--- web/garden/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Howjoy::Application.config.secret_token = '1f878891bc9c9b5d48c3265281cc5e11d3214ae159b4cbf1929691b5de77f1d41ff2077d4ed52d2f78548851554d986794dffefafdf30cebb89d72405d189131'
